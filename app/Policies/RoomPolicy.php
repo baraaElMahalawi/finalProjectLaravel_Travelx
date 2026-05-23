fc36689e5a909de241fe02f6c29fa0728d77e55d
Baraa Osama ElMahalawi <baraaelmahalawi@mbpalkhasbbaraa.mynet>
2026-05-23 13:57:27 +0300

    Initial commit: Laravel project structure

--- app/Policies/RoomPolicy.php
+++ app/Policies/RoomPolicy.php
@@ -8,38 +8,39 @@ use App\Models\Room;
 class RoomPolicy
 {
 
-      //تحديد ما إذا كان المستخدم يمكنه عرض قائمة الغرف
-    public function viewAny(User \$user)
+      //تحديد ما إذا كان المستخدم يمكنه عرض قائمة الغرف 
+
+    public function viewAny(User $user)
     {
         // السماح فقط للأدمن
-        return \$user->isAdmin();
+        return $user->isAdmin();
     }
 
     
      // تحديد ما إذا كان المستخدم يمكنه عرض غرفة معينة
-    public function view(User \$user, Room \$room)
+    public function view(User $user, Room $room)
     {
-        return \$user->isAdmin();
+        return $user->isAdmin();
     }
 
     
      // تحديد ما إذا كان المستخدم يمكنه إنشاء غرفة جديدة
-    public function create(User \$user)
+    public function create(User $user)
     {
-        return \$user->isAdmin();
+        return $user->isAdmin();
     }
 
     
      // تحديد ما إذا كان المستخدم يمكنه تحديث غرفة معينة
-    public function update(User \$user, Room \$room)
+    public function update(User $user, Room $room)
     {
-        return \$user->isAdmin();
+        return $user->isAdmin();
     }
 
     
     // تحديد ما إذا كان المستخدم يمكنه حذف غرفة معينة
-    public function delete(User \$user, Room \$room)
+    public function delete(User $user, Room $room)
     {
-        return \$user->isAdmin();
+        return $user->isAdmin();
     }
 }
